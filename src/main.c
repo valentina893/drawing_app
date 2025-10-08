1e3f185661946aa94d175217299f04ee0a888ff5
Valentina <zenit@Valentinas-MacBook-Air.local>
2025-10-08 15:43:31 -0500

    fixed spotty draw strokes

--- src/main.c
+++ src/main.c
@@ -5,21 +5,33 @@
 #define window_width 640
 #define window_height 480
 
+void drawLineOfCircles(renderer* renderer, int x, int y, int prev_x, int prev_y) {
+    int dx = x - prev_x;
+    int dy = y - prev_y;
+    int steps = SDL_max(abs(dx), abs(dy));
+    for (int i = 0; i <= steps; i++) {
+        int xi = prev_x + dx * i / steps;
+        int yi = prev_y + dy * i / steps;
+        renderer_drawFilledCircle(renderer, xi, yi, 2, 0, 0, 0, 255);
+    }
+}
+
 int main() {
 
     renderer renderer;
     renderer_create(&renderer, "draw", window_width, window_height);
 
-    int click = 0;
-
     int running = 1;
     SDL_Event event;
 
+    int click = 0;
+    int prev_x = -1, prev_y = -1;
+
     SDL_Texture* canvas = SDL_CreateTexture(
-    renderer._renderer,
-    SDL_PIXELFORMAT_RGBA8888,
-    SDL_TEXTUREACCESS_TARGET,
-    window_width, window_height
+        renderer._renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_TARGET,
+        window_width, window_height
     );
 
     SDL_SetRenderTarget(renderer._renderer, canvas);
@@ -34,20 +46,33 @@ int main() {
                 break;
             case SDL_MOUSEBUTTONDOWN:
                 click = 1;
+                SDL_GetMouseState(&prev_x, &prev_y);  // start stroke
                 break;
             case SDL_MOUSEBUTTONUP:
                 click = 0;
+                prev_x = prev_y = -1;                 // reset stroke
                 break;
             default:
                 break;
             }
         }
+
         if (click) {
             int x, y;
             SDL_GetMouseState(&x, &y);
+
             SDL_SetRenderTarget(renderer._renderer, canvas);
-            renderer_drawFilledCircle(&renderer, x, y, 2, 0, 0, 0, 255);
+
+            if (prev_x >= 0 && prev_y >= 0) {
+                drawLineOfCircles(&renderer, x, y, prev_x, prev_y);
+            } else {
+                renderer_drawFilledCircle(&renderer, x, y, 2, 0, 0, 0, 255);
+            }
+
             SDL_SetRenderTarget(renderer._renderer, NULL);
+
+            prev_x = x;
+            prev_y = y;
         }
         SDL_RenderCopy(renderer._renderer, canvas, NULL, NULL);
         renderer_present(&renderer);
